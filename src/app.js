047b432968f0c68a15a1f9c413fea7e694f35cd2
Wellingtonalves33 <wellingtonvdtc@gmail.com>
2024-12-30 01:18:13 -0300

    tualiza configuração CORS e middlewares - 02

--- src/app.js
+++ src/app.js
@@ -8,19 +8,38 @@ class App {
     constructor() {
         this.app = express()
         
-        this.app.use(cors());
+        // Configuração atualizada do CORS
+        this.app.use(cors({
+            origin: [
+                'https://devburger-front-c5gfmkp2i-wellingtonalves33s-projects.vercel.app',
+                'http://localhost:5173'  // para desenvolvimento local
+            ],
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+            allowedHeaders: ['Content-Type', 'Authorization']
+        }));
+
         this.middlewares();
         this.routes();
     };
+
     middlewares() {
         this.app.use(express.json());
-        this.app.use('/product-file', express.static(resolve(__dirname, '..', 'uploads')),
-    );
-        this.app.use('/category-file', express.static(resolve(__dirname, '..', 'uploads')),);
+        
+        // Configuração dos arquivos estáticos
+        const uploadsPath = resolve(__dirname, '..', 'uploads');
+        this.app.use('/product-file', express.static(uploadsPath));
+        this.app.use('/category-file', express.static(uploadsPath));
+
+        // Adicione um middleware para log de requisições (opcional, mas útil para debug)
+        this.app.use((req, res, next) => {
+            console.log(`${req.method} ${req.path}`);
+            next();
+        });
     };
+
     routes() {
-        this.app.use(routes)
+        this.app.use(routes);
     };
 }
 
-export default new App().app
+export default new App().app;
