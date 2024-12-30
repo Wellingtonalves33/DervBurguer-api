fcdd338d0d2c3eb60dafe96afa644248a0f83799
Wellingtonalves33 <wellingtonvdtc@gmail.com>
2024-12-30 12:12:42 -0300

    correçao do projeto

--- src/app.js
+++ src/app.js
@@ -1,83 +1,26 @@
-import express from 'express';
-import routes from './routes';
-import { resolve } from 'node:path';
-import cors from 'cors';
+import express from 'express'
+import { resolve } from 'node:path'
+import routes from './routes'
+import cors from 'cors'
 import './database';
 
 class App {
     constructor() {
-        try {
-            console.log('Iniciando aplicação...');
-            this.app = express();
+        this.app = express();
+        this.app.use(cors());
+        this.middlewares();
+        this.routes();
+    }
+    middlewares() {this.app.use(express.json())
+    this.app.use('/product-file', express.static(resolve(__dirname, '..', 'uploads')));
 
-            // Configuração básica do express
-            this.app.use(express.json());
-            console.log('Express JSON configurado');
-
-            // Configuração do CORS
-            const corsOptions = {
-                origin: [
-                    'https://devburger-front-k5ky1j94h-wellingtonalves33s-projects.vercel.app',
-                    'http://localhost:3000',
-                    'https://devburger-front-end.vercel.app'
-                ],
-                credentials: true,
-                optionsSuccessStatus: 204,
-                methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-                allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
-            };
-
-            this.app.use(cors(corsOptions));
-            console.log('CORS configurado');
-
-            this.middlewares();
-            this.routes();
-            console.log('Aplicação iniciada com sucesso');
-        } catch (error) {
-            console.error('Erro ao iniciar aplicação:', error);
-        }
+    this.app.use('/category-file', express.static(resolve(__dirname, '..', 'uploads')));
     }
 
-    middlewares() {
-        try {
-            const uploadsPath = resolve(__dirname, '..', 'uploads');
-            this.app.use('/product-file', express.static(uploadsPath));
-            this.app.use('/category-file', express.static(uploadsPath));
-
-            // Error handling middleware
-            this.app.use((err, req, res, next) => {
-                console.error(err.stack);
-                res.status(500).json({ error: 'Algo deu errado!' });
-            });
-
-            console.log('Middlewares configurados');
-        } catch (error) {
-            console.error('Erro nos middlewares:', error);
-        }
-    }
 
     routes() {
-        try {
-            // Rota de teste
-            this.app.get('/test', (req, res) => {
-                res.json({ message: 'API is working!' });
-            });
-
-            this.app.use(routes);
-            console.log('Rotas configuradas');
-        } catch (error) {
-            console.error('Erro nas rotas:', error);
-        }
+        this.app.use(routes)
     }
 }
 
-// Tratamento de erros não capturados
-process.on('unhandledRejection', (reason, promise) => {
-    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
-});
-
-process.on('uncaughtException', (error) => {
-    console.error('Uncaught Exception:', error);
-});
-
-export default new App().app;
+export default new App().app
--- src/database/index.js
+++ src/database/index.js
@@ -1,80 +1,32 @@
-import Sequelize from "sequelize";
+
+
+import  Sequelize  from "sequelize";
 import mongoose from "mongoose";
-import configDatabase from '../config/database';
-import 'dotenv/config';
+
+import configDatabase from '../config/database'
+
+import User from '../app/models/User';
+import Product from "../app/models/Product";
+import Category from "../app/models/Category";
+
+const models = [User, Product,Category];
+
 
 class Database {
-    constructor() {
-        try {
-            console.log('Iniciando conexão com banco de dados...');
-            console.log('Config Database:', JSON.stringify(configDatabase, null, 2));
-            this.init();
-            this.mongo();
-        } catch (error) {
-            console.error('Erro no constructor do Database:', error);
-            throw error;
-        }
-    }
-    
-    async init() {
-        try {
-            console.log('Tentando conectar ao PostgreSQL...');
-            this.connection = new Sequelize(
-                process.env.NODE_ENV === 'production'
-                    ? process.env.DATABASE_URL
-                    : configDatabase
-            );
-            await this.connection.authenticate();
-            console.log('Conexão PostgreSQL estabelecida com sucesso');
-        } catch (error) {
-            console.error('Erro na conexão PostgreSQL:', error);
-            throw error;
-        }
-    }
-    
-    async mongo() {
-        try {
-            console.log('Tentando conectar ao MongoDB...');
-            const mongoURL = process.env.MONGODB_URI;
-            console.log('MongoDB URL:', mongoURL ? 'URL configurada' : 'URL não encontrada');
-            await mongoose.connect(mongoURL);
-            console.log('Conexão MongoDB estabelecida com sucesso');
-        } catch (error) {
-            console.error('Erro na conexão MongoDB:', error);
-            throw error;
-        }
-    }
+   constructor(){
+       this.init();
+       this.mongo();
+   }
+   init(){
+       this.connection = new Sequelize(configDatabase);
+       models
+       .map((model)=> model.init(this.connection))
+       .map(
+           (model) => model.associate && model.associate(this.connection.models),
+       );
+   }
+   mongo() {
+       this.mongoConnection = mongoose.connect('mongodb://localhost:27017/devburger');
+   }
 }
-
 export default new Database();
-
-// import  Sequelize  from "sequelize";
-// import mongoose from "mongoose";
-
-// import configDatabase from '../config/database'
-
-// import User from '../app/models/User';
-// import Product from "../app/models/Product";
-// import Category from "../app/models/Category";
-
-// const models = [User, Product,Category];
-
-
-// class Database {
-//    constructor(){
-//        this.init();
-//        this.mongo();
-//    }
-//    init(){
-//        this.connection = new Sequelize(configDatabase);
-//        models
-//        .map((model)=> model.init(this.connection))
-//        .map(
-//            (model) => model.associate && model.associate(this.connection.models),
-//        );
-//    }
-//    mongo() {
-//        this.mongoConnection = mongoose.connect('mongodb://localhost:27017/devburger');
-//    }
-// }
-// export default new Database();
