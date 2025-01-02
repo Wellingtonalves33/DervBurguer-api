138e9a1e8ff1e20bea4ad0b670ce83b391a63890
Wellingtonalves33 <wellingtonvdtc@gmail.com>
2025-01-02 15:00:57 -0300

    preparando api

--- src/server.js
+++ src/server.js
@@ -1,7 +1,4 @@
-import app from './app';
+import app from "./app"
 
-const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-    console.log(`Server is running at port ${PORT}...`);
-});
+app.listen(3001, () => console.log('Server is running at port 3001...'))
