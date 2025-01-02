cdd8faf5d39a5c07d5d1239a77716d6273caa30f
Wellingtonalves33 <wellingtonvdtc@gmail.com>
2025-01-02 16:24:54 -0300

    atualização

--- src/config/database.js
+++ src/config/database.js
@@ -1,38 +1,16 @@
+
+
+
 module.exports = {
-  dialect: 'postgres',
-  host: process.env.POSTGRES_HOST || 'localhost',
-  username: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.POSTGRES_PASSWORD || 'postgres',
-  database: process.env.POSTGRES_DB || 'devburger',
-  port: process.env.POSTGRES_PORT || 5432,
-  url: process.env.DATABASE_URL,
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
-  },
-  dialectOptions: process.env.NODE_ENV === 'production' 
-    ? {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-      } 
-    : {}
+    dialect:'postgres',
+    host:'localhost',
+    port: 5432,
+    username:'postgres',
+    password: 'postgres',
+    database:'devburger',
+    define:{
+        timestamps: true,
+        underscored: true,
+        underscoredAll:true,
+    },
 };
-
-
-
-// module.exports = {
-//     dialect:'postgres',
-//     host:'localhost',
-//     port: 5432,
-//     username:'postgres',
-//     password: 'postgres',
-//     database:'devburger',
-//     define:{
-//         timestamps: true,
-//         underscored: true,
-//         underscoredAll:true,
-//     },
-// };
